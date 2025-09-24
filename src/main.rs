a0c6c13ebccae1166e65bbac7afd1b1e01ea4011
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-09-24 12:29:12 +0800

    feat(main.rs): 以硬币为例子，写一个可以根据硬币返回数值的函数

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use std::collections::binary_heap::PeekMut;
+
 // rust提供了一个非常强大的控制流运算符match
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let penny = Coin::Penny;
+    println!("{}", value_in_cents(penny));
 }
